dbf5c80bd9eee88a4deccf584dc3c212bc164263
Donaldo <49104068+DgameMod@users.noreply.github.com>
2025-05-30 08:05:01 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './index.css';
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Products from "./components/Products";
 
 function App() {
   return (
     <div className="font-sans">
       <Hero />
       <About />
+      <Products />
     </div>
   );
 }
