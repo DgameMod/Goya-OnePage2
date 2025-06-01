a971729b117aed4304ce40ec25341b9b02bfc620
Donaldo <49104068+DgameMod@users.noreply.github.com>
2025-05-31 20:07:12 -0600

    Update Gallery.jsx

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -19,7 +19,7 @@ function Gallery() {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 max-w-6xl mx-auto">
         {imagenes.map((src, index) => (
-          <motion.div
+         <motion.div
             key={index}
             className="overflow-hidden rounded-xl shadow"
             initial={{ opacity: 0, scale: 0.95 }}
