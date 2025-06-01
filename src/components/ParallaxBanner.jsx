ae9ae50ee1d59e4c76e9688a23e5772ed3c3a415
Donaldo <49104068+DgameMod@users.noreply.github.com>
2025-05-31 22:05:24 -0600

    Update ParallaxBanner.jsx

--- src/components/ParallaxBanner.jsx
+++ src/components/ParallaxBanner.jsx
@@ -17,7 +17,7 @@ function ParallaxBanner() {
       {/* Fondo parallax */}
       <motion.div style={{ y }} className="absolute inset-0 z-0">
         <img
-          src="/parallax-bg.png"
+          src="/parallax-brochas-banner.png"
           alt="Calidad GOYA"
           className="w-full h-full object-cover"
         />
