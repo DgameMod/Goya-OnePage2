fbb66e7ef751d4fd3c33fe28e99e2dda3dd1d1ea
Donaldo <49104068+DgameMod@users.noreply.github.com>
2025-06-10 00:48:33 -0600

    Update Gallery.jsx

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -19,20 +19,20 @@ function Gallery() {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 max-w-6xl mx-auto">
         {imagenes.map((src, index) => (
-         <motion.div
-            key={index}
-            className="overflow-hidden rounded-xl shadow"
-            initial={{ opacity: 0, scale: 0.95 }}
-            whileInView={{ opacity: 1, scale: 1 }}
-            transition={{ duration: 0.3, delay: index * 0.1 }}
-            viewport={{ once: true }}
-          >
-            <img
-              src={src}
-              alt={`Galería ${index + 1}`}
-              className="w-full h-60 object-cover hover:scale-105 transition-transform duration-300"
-            />
-          </motion.div>
+              <motion.div
+          key={index}
+          className="overflow-hidden rounded-xl shadow bg-blue-50 border border-blue-100"
+          initial={{ opacity: 0, scale: 0.95 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.3, delay: index * 0.1 }}
+          viewport={{ once: true }}
+        >
+          <img
+            src={src}
+            alt={`Galería ${index + 1}`}
+            className="w-full h-60 object-cover hover:scale-105 transition-transform duration-300"
+          />
+        </motion.div>
         ))}
       </div>
     </section>
